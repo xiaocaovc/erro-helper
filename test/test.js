bbf60f2cb4eb017f268cb53062e44daba10821f9
centercao <centercao2010@163.com>
2017-09-04 10:38:53 +0800

    修改内部结构，构成code + message

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var ApiError = require('../index');
-ApiError.NOT_REGISTER = "not register";
-ApiError.set(ApiError.NOT_REGISTER,10,"位注册!");
+ApiError.NOT_REGISTER = 10;
+ApiError.set(ApiError.NOT_REGISTER,"位注册!");
 
 var err = new ApiError(ApiError.NOT_REGISTER);
 console.log(err.message);
